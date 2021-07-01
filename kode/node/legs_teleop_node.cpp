396af919dfa45379b00d04b6c25abcec928c7d74
Alfi Maulana <alfi.maulana.f@gmail.com>
2021-07-01 21:45:54 +0700

    feat: fill legs teleop node

---
+++ kode/node/legs_teleop_node.cpp
@@ -0,0 +1,25 @@
+#include <beine_cpp/beine_cpp.hpp>
+#include <rclcpp/rclcpp.hpp>
+
+...
+
+int main(int argc, char ** argv)
+{
+  ...
+
+  rclcpp::init(argc, argv);
+
+  auto node = ...;
+  auto legs_provider = ...;
+
+  auto update_timer = node->create_wall_timer(
+    100ms, [&]() {
+      ...
+    });
+
+  rclcpp::spin(node);
+
+  rclcpp::shutdown();
+
+  return 0;
+}
